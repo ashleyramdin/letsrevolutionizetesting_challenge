f974a73761e0409d68c03fec5d16678424c135bb
ashleyramdin <ashleyramdin@outlook.com>
2023-09-23 17:27:36 -0400

    refactored code in letsrevolutionizetesting_solution.py

--- letsrevolutionizetesting_solution.py
+++ letsrevolutionizetesting_solution.py
@@ -3,7 +3,7 @@ import requests
 
 def follow_challenge_url(url):
     response = requests.get(url)
-    
+
     if response.status_code != 200:
         print(f"Failed to fetch data from {url}")
         return
@@ -11,23 +11,15 @@ def follow_challenge_url(url):
     # get the json response
     data = response.json()
     print(data)
-    
-    if data["message"] == "This is not the end":  # end condition
-        # update the url to include .json
-        json_url = data["follow"].replace("challenge", "challenge.json")
 
-        # split the url into parts to get the id parameter for the next url
-        url_parts = json_url.split("=")
-        pre_query_string = url_parts[0]  # first part of the url
-        new_id = url_parts[1]  # id parameter
-
-        # construct the new follow url
-        follow_url = pre_query_string.replace("?id", f"?id={new_id}")
-
-        # print(f"Follow URL: {follow_url}")
-
-        # call the function recursively
-        follow_challenge_url(follow_url)
+    if data.get("message") == "This is not the end":
+        follow_url = data.get("follow", "")
+        if follow_url:
+            # get the id parameter for the next url
+            new_id = follow_url.split("=")[-1]
+            # construct the new follow url
+            follow_url = follow_url.replace("challenge", f"challenge.json?id={new_id}")
+            follow_challenge_url(follow_url)
 
 
 if __name__ == "__main__":
